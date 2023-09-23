d96bc5a6f6aa764121d87eed92e138962ecc338c
MoonYeongSang <118742845+MoonYeongSang@users.noreply.github.com>
2023-09-24 00:40:46 +0900

    POST /api/v1/record 구현 ApiResponseGenerator.SuccessBodyWithNoData 구현

--- src/main/java/com/example/demo/domain/usecase/record/RecordService.java
+++ src/main/java/com/example/demo/domain/usecase/record/RecordService.java
@@ -3,7 +3,8 @@ package com.example.demo.domain.usecase.record;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.entity.RecordEntity;
 import com.example.demo.repository.repository.RecordRepository;
-import com.example.demo.web.dto.request.RecordRequest;
+import com.example.demo.web.dto.request.RecordRequestSave;
+import com.example.demo.web.dto.request.RecordRequestUpdate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,7 @@ public class RecordService {
 
     private final RecordRepository recordRepository;
 
-    public RecordEntity saveRecord(RecordRequest recordRequest) {
+    public RecordEntity saveRecord(RecordRequestSave recordRequest) {
         RecordEntity recordEntity = new RecordEntity();
         recordEntity.setTitle(recordRequest.getTitle());
         recordEntity.setContent(recordRequest.getContent());
@@ -32,19 +33,20 @@ public class RecordService {
         }
     }
 
-    public RecordEntity update(Long id, RecordRequest recordRequest) {
+    public RecordEntity update( RecordRequestUpdate recordRequest) {
 
-        RecordEntity recordEntity = null;
-        Optional<RecordEntity> findRecordEntity = recordRepository.findById(id);
-        if (findRecordEntity.isPresent()) {
-            recordEntity= findRecordEntity.get();
-        } else {
-            throw new ResourceNotFoundException("없는 id 입니다.");
-        }
+        RecordEntity recordEntity = getRecordById(recordRequest.getId());
+
         recordEntity.setTitle(recordRequest.getTitle());
         recordEntity.setContent(recordRequest.getContent());
 
         return recordEntity;
+    }
+
+    public void delete(Long id) {
+        RecordEntity findRecord = getRecordById(id);
+        recordRepository.delete(findRecord);
+
     }
 
 
--- src/main/java/com/example/demo/web/dto/request/RecordRequestSave.java
+++ src/main/java/com/example/demo/web/dto/request/RecordRequestSave.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class RecordRequest {
+public class RecordRequestSave {
 
 
     private String title;
